c7e41969ab56164a1cf3e6bf49570f781721a1d9
Mishka <mishka.s.mukherji@gmail.com>
2015-03-03 17:06:51 -0500

    lists are finally linked to users

--- app/controllers/to_do_lists_controller.rb
+++ app/controllers/to_do_lists_controller.rb
@@ -14,14 +14,14 @@ class ToDoListsController < ApplicationController
   end
 
   def new_list
+    # user = User.find(params[:id]) 
     @list = ToDoList.new
     render :new_list
   end
 
   def add_list
     list_params = params[:to_do_list]
-    binding.pry
-    @list = ToDoList.new name: list_params[:name] 
+    @list = ToDoList.new name: list_params[:name], user_id: current_user.id
 
     @list.save!
     redirect_to '/'
